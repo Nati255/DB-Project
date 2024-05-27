323c72c35596d856f999c0ea52914484bc5a40ee
daniel_cohen <daniel1cohen13579@gmail.com>
2024-05-27 19:24:43 +0300

    Add files for stage one

---
+++ Stage1/createTables.sql
@@ -0,0 +1,81 @@
+CREATE TABLE Student
+(
+  First_Name VARCHAR(10) NOT NULL,
+  Student_ID INT NOT NULL,
+  Date_Birth DATE NOT NULL,
+  Gradesheats INT NOT NULL,
+  Address VARCHAR(20) NOT NULL,
+  Last_Name VARCHAR(10) NOT NULL,
+  PRIMARY KEY (Student_ID)
+);
+
+CREATE TABLE Teacher
+(
+  Teacher_ID INT NOT NULL,
+  L_Name VARCHAR(10) NOT NULL,
+  F_Name VARCHAR(10) NOT NULL,
+  Title VARCHAR(10) NOT NULL,
+  Level INT NOT NULL,
+  PRIMARY KEY (Teacher_ID)
+);
+
+CREATE TABLE Subject
+(
+  Subject_ID INT NOT NULL,
+  Name VARCHAR(10) NOT NULL,
+  Detail VARCHAR(10) NOT NULL,
+  PRIMARY KEY (Subject_ID)
+);
+
+CREATE TABLE Classroom
+(
+  Room_Type VARCHAR(10) NOT NULL,
+  Capacity INT NOT NULL,
+  Classroom_ID INT NOT NULL,
+  PRIMARY KEY (Classroom_ID)
+);
+
+CREATE TABLE TeachAssign
+(
+  DateAssign VARCHAR(10) NOT NULL,
+  Subject_ID INT NOT NULL,
+  Teacher_ID INT NOT NULL,
+  PRIMARY KEY (Subject_ID, Teacher_ID),
+  FOREIGN KEY (Subject_ID) REFERENCES Subject(Subject_ID),
+  FOREIGN KEY (Teacher_ID) REFERENCES Teacher(Teacher_ID)
+);
+
+CREATE TABLE Class
+(
+  Class_ID INT NOT NULL,
+  PERIODD VARCHAR(10) NOT NULL,
+  Time VARCHAR(10) NOT NULL,
+  Classroom_ID INT NOT NULL,
+  Subject_ID INT NOT NULL,
+  Teacher_ID INT NOT NULL,
+  PRIMARY KEY (Class_ID),
+  FOREIGN KEY (Classroom_ID) REFERENCES Classroom(Classroom_ID),
+  FOREIGN KEY (Subject_ID) REFERENCES Subject(Subject_ID),
+  FOREIGN KEY (Teacher_ID) REFERENCES Teacher(Teacher_ID)
+);
+
+CREATE TABLE Schedule
+(
+  ClassDate DATE NOT NULL,
+  Hour VARCHAR(10) NOT NULL,
+  Schedule_ID INT NOT NULL,
+  Student_ID INT NOT NULL,
+  Class_ID INT NOT NULL,
+  PRIMARY KEY (Schedule_ID),
+  FOREIGN KEY (Student_ID) REFERENCES Student(Student_ID),
+  FOREIGN KEY (Class_ID) REFERENCES Class(Class_ID)
+);
+
+CREATE TABLE StudentClass
+(
+  Student_ID INT NOT NULL,
+  Class_ID INT NOT NULL,
+  PRIMARY KEY (Student_ID, Class_ID),
+  FOREIGN KEY (Student_ID) REFERENCES Student(Student_ID),
+  FOREIGN KEY (Class_ID) REFERENCES Class(Class_ID)
+);
---
+++ Stage1/selectAll.sql
@@ -0,0 +1,16 @@
+-- Select statements for retrieving data from each table
+SELECT * FROM Student;
+
+SELECT * FROM Teacher;
+
+SELECT * FROM Subject;
+
+SELECT * FROM Classroom;
+
+SELECT * FROM TeachAssign;
+
+SELECT * FROM Class;
+
+SELECT * FROM Schedule;
+
+SELECT * FROM StudentClass;
